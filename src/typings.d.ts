38bcc74286cee0cfa2bb543bce67a88258ffe886
171035726@qq.com <171035726@qq.com>
2022-04-06 15:05:37 +0800

    chore: update webpack

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,13 +1,13 @@
-declare module '*.css';
-declare module '*.less';
-declare module '*.scss';
-declare module '*.sass';
-declare module '*.svg';
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.gif';
-declare module '*.bmp';
-declare module '*.tiff';
+declare module "*.css";
+declare module "*.less";
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.bmp";
+declare module "*.tiff";
 
-declare const REACT_APP_ENV: 'test' | 'dev' | 'pre' | false;
+declare const REACT_APP_ENV: "test" | "dev" | "prod" | false;
